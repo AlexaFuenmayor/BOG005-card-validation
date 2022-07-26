1936703911617d18f620438fe2adbe512d54ac95
AlexaFuenmayor <estherf.m18@gmail.com>
2022-07-25 23:06:53 -0500

    cree la estructura html con algo de css luego la funcion isvalid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+const botonn = document.getElementById("button");
+
+let input = document.getElementById("card");
+
+function saveN() {
+ // document.getElementById("result").style.visibility = "";
+  let cardN = document.getElementById("card").value;
+  //capturar en la variable cardN lo que el usuario digite en el campo input
+  if (validator.isValid(cardN)) {
+    document.getElementById("form").style.backgroundColor= "green",
+    document.getElementById("valid").innerHTML= "   T A R J E T A    V Á L I D A";
+
+  } else {
+    document.getElementById("form").style.backgroundColor= "red";
+    document.getElementById("valid").innerHTML= "T A R J E T A    I N V Á L I D A";
+  }
+  let card = document.getElementById("card").value;
+  //validator.maskify(card)
+
+  document.getElementById("card").value = validator.maskify(card);
+}
+function justNumber(event) {
+
+  event.preventDefault();
+  let codigoKey = event.keyCode;
+  let numberCard = String.fromCharCode(codigoKey);
+  let valor = parseInt(numberCard);
+
+  if (valor) {
+    input.value += valor;
+  } else {
+    alert("Ingrese solo números");
+  }
+}
+input.addEventListener("keypress", justNumber);
+botonn.addEventListener("click", saveN);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,44 @@
 const validator = {
-  // ...
+  isValid: function (creditCardNumber) {
+    let numberFinal = 0;
+    let numberPar = 0;
+    let b = false;
+
+    for (let n = creditCardNumber.length -1; n >= 0; n--) {
+      let cDigit = creditCardNumber.charAt(n);
+      numberPar = parseInt(cDigit);
+
+      if (b) {
+        if ((numberPar *= 2) > 9) numberPar -= 9;
+      }
+      numberFinal += numberPar;
+      b = !b;
+    }
+    return numberFinal % 10 == 0;
+  }, 
+  
+
+  maskify: function (creditCardNumber) {
+    let string1 = " ";
+    let string2 = " ";
+    let valores = " ";
+
+    //   console.log(card.value.length)
+    if (creditCardNumber.length >= 4) {
+      string1 = creditCardNumber.substring(
+        creditCardNumber.length - 4,
+        creditCardNumber.length
+      );
+      string2 = creditCardNumber
+        .substring(0, creditCardNumber.length - 4)
+        .replace(/\w/g, "#");
+      valores = string2 + string1;
+
+      return valores;
+    }else{
+      return creditCardNumber;
+    }
+  },
 };
 
 export default validator;
